54e2baeebde0c33278d371086f3a847fbf15c8b1
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-03 21:46:13 +0530

    Hacker Rank

--- FindString.py
+++ FindString.py
@@ -5,12 +5,15 @@ s = input("Enter another string(subpart of string inputted above) :- ")
 length = len(s)
 words = []
 ans = 0
+
 for n in range(0, len(st)):
   word = st[n:n+length]
   words.append(word)
+
 for i in words:
   if i == s:
     ans += 1
+
 print(ans)
 
 #Done
