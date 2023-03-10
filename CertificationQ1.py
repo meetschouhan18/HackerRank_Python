6cdc98e72f827de62f31bdc7bf13952973820431
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-10 22:19:11 +0530

    Hacker Rank Python Certification

--- CertificationQ1.py
+++ CertificationQ1.py
@@ -1,15 +1,20 @@
 def avg(n):
     sum = 0
     c = 0
+    
     for i in range(-100, 100):
         if i in n :
             c = c + 1
             sum = sum + i
+    
     average = sum/c
     return average
+
 nums = list(map(int, input().split()))
+
 if len(nums) == 1:
     print(str(nums[0])+".00")
+
 else:
     res = avg(nums)
     print(res)
