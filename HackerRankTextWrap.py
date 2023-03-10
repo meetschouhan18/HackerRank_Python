fdb3a3187b1d46840d4111a0288e1d9f50aad858
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-10 22:16:36 +0530

    Add files via upload

---
+++ HackerRankTextWrap.py
@@ -0,0 +1,13 @@
+#Text Wrap
+#break string at a given index everytime and print the sub-string
+
+string = input("")
+width = int(input(""))
+c = 0
+l = []
+while c < len(string):
+    s = string[c:c + width]
+    c = c + width
+    l.append(s + '\n')
+st = "".join(l)
+print(st)
