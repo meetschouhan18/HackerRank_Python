8f97df3a9c894d0872d71a7828a4b51044c98c90
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-10 22:18:04 +0530

    Hacker Rank

--- TextWrap.py
+++ TextWrap.py
@@ -5,9 +5,11 @@ string = input("")
 width = int(input(""))
 c = 0
 l = []
+
 while c < len(string):
     s = string[c:c + width]
     c = c + width
     l.append(s + '\n')
+
 st = "".join(l)
 print(st)
