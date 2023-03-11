a3847359a9fbde018da5e903e87da4fbd3fc8c4e
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-11 15:46:14 +0530

    Add files via upload

---
+++ HackerRankCertificationQ2.py
@@ -0,0 +1,20 @@
+def reverse_words_order_and_swap_cases(sentence):
+    # Write your code here
+    a = []
+    b = []
+    n = sentence.split()
+    n.reverse()
+    for i in n:
+        for j in i:
+            if j.isupper():
+                j = j.lower()
+            else:
+                j = j.upper()
+            a.append(j)
+        b.append("".join(a))
+        a.clear()    
+    return " ".join(b)
+if __name__ == '__main__':
+    sentence = input()
+    result = reverse_words_order_and_swap_cases(sentence)
+    print(result)
