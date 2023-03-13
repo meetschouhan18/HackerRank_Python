315fad29c2d20d388ffd5b3f6363c720a5026e2a
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-13 14:56:19 +0530

    Hacker Rank

--- RunnerUpScore.py
+++ RunnerUpScore.py
@@ -6,13 +6,16 @@ a = input('Enter score :- ')
 d = []
 r = []
 c = a.split()
+
 for i in c:
     d.append(int(i))
     r.append(int(i))
 e = max(r)
+
 for i in r:
     if i == e:
         d.remove(i)
+
 try:
     print(max(d))
 except:
