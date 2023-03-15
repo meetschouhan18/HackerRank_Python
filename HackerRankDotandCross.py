1be717ac3f17042eb5f97d38185b583aff73a3cb
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-15 14:32:09 +0530

    Add files via upload

---
+++ HackerRankDotandCross.py
@@ -0,0 +1,18 @@
+import numpy as np
+n = int(input())
+lst_a = []
+lst_b = []
+for i in range(0 ,n):
+   line = [*map(int , input().split())]
+   for j in line:
+      lst_a.append(j)
+for i in range(0 ,n):
+   line = [*map(int , input().split())]
+   for j in line:
+      lst_b.append(j)
+a = np.array(lst_a)
+a.resize(n,n)
+b = np.array(lst_b)
+b.resize(n,n)
+print(np.dot(a,b))
+print(np.cross(a,b))
---
+++ HackerRankFloorCeilandRint.py
@@ -0,0 +1,6 @@
+import numpy as np
+np.set_printoptions(sign=" ")
+x = np.array([*map(float,input().split())])
+print(np.floor(x))
+print(np.ceil(x))
+print(np.rint(x))
