f96065b8b66e4562b01fadf4da995dc22f89d217
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-17 15:20:54 +0530

    Hacker Rank

--- MeanVarandStd.py
+++ MeanVarandStd.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 np.set_printoptions(legacy = "1.13",sign = " ")
 n,m = map(int , input().split())
 l = []
@@ -6,6 +7,7 @@ for i in range(0 ,n):
    lst = [*map(int , input().split())]
    for j in lst:
       l.append(j)
+
 x = np.array(l)
 x.resize(n,m)
 print(np.mean(x , axis = 1))
