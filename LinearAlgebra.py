7840f6299f2b81866eba38b3bc83c6398ac9c7fc
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-17 15:19:30 +0530

    Hacker Rank

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -1,10 +1,12 @@
 import numpy as np
+
 n = int(input())
 l = []
 for i in range(0 ,n):
    line = [*map(float , input().split())]
    for j in line:
       l.append(j)
+
 x = np.array(l)
 x.resize(n,n)
 b = np.linalg.det(x)
