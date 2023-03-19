efcbd1111ca62eb351788eac8caf9b1ff2d65422
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-19 10:13:39 +0530

    Hacker Rank

--- ZerosAndOnes.py
+++ ZerosAndOnes.py
@@ -1,18 +1,22 @@
 import numpy as np
+
 line = input().split()
 n = int(line[0])
 m = int(line[1])
+
 if len(line) == 2:
    a = np.zeros([n,m],int)
    b = np.ones([n,m],int)
    print(a)
    print(b)
+
 elif len(line) == 3:
    p = int(line[2])
    a = np.zeros([n,m,p],int)
    b = np.ones([n,m,p],int)
    print(a)
    print(b)
+
 else:
    p = int(line[2])
    q = int(line[3])
