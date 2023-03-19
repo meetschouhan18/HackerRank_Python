8b77aa4c918845bf5bc6ce980dd6c2fca6d164b7
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-19 10:12:56 +0530

    Hacker Rank

--- TransposeAndFlatten.py
+++ TransposeAndFlatten.py
@@ -1,12 +1,15 @@
 import numpy as np
+
 lst = []
 line = input().split()
 n = int(line[0])
 m = int(line[1])
+
 for i in range(0 ,int(line[0])):
    line = input().split()
    for j in line:
       lst.append(int(j))
+
 x = np.array(lst)
 y = x.reshape(n,m)
 print(np.transpose(y))
