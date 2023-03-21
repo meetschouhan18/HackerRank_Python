f6e7f7ec1ea087476ce50ec7abc4bce84e3d6afa
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-21 22:34:42 +0530

    Hacker Rank

--- IntroductionToSets.py
+++ IntroductionToSets.py
@@ -1,14 +1,18 @@
 def average(array):
     # your code goes here
+    
     total = set()
     sum = 0
     num = array
     for i in num:
         total.add(int(i))
+    
     for i in total:
         sum = sum + int(i)
+    
     avg = sum/len(total)
     return avg
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
