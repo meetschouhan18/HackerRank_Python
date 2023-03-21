d2efb0e8e0701af19cda5e58f784411ccc2209e9
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-21 22:33:22 +0530

    Hacker Rank

--- CheckStrictSuperset.py
+++ CheckStrictSuperset.py
@@ -2,18 +2,23 @@ first_set = set()
 second_set = set()
 line = input()
 l = line.split()
+
 for j in l:
     first_set.add(int(j))
+
 n = int(input())
 p = 0
+
 for i in range(0 , n):
     line1 = input()
     l1 = line1.split()
     for j in l1:
         second_set.add(int(j))
+
 for j in second_set:
     if j in first_set:
         p += 1
+        
 if p == len(second_set):
     print("True")
 else:
