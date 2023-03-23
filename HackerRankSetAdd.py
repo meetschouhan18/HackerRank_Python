df3ea45dddc504bf61367433036e57af55d8fba3
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-23 18:41:10 +0530

    Add files via upload

---
+++ HackerRankSetAdd.py
@@ -0,0 +1,6 @@
+n = int(input())
+stamps = set()
+for i in range(0 , n):
+    name = input()
+    stamps.add(name)
+print(len(stamps))
---
+++ HackerRankSetDiscardRemovePop.py
@@ -0,0 +1,35 @@
+first_set = set()
+b = 0
+second_set = set()
+n = int(input())
+first_line = input()
+first_num = first_line.split()
+for i in first_num:
+    first_set.add(int(i))
+m = int(input())
+for i in range(0 , m):
+    operation = input()
+    op = operation.split()
+    if op[0] != "pop":
+        try:
+            a = int(op[1])
+        except:
+            for i in first_set:
+                b = b + int(i)
+            print(b)
+            break
+    if op[0] == "pop":
+        c = first_set.pop()
+    if op[0] == "remove":
+        first_set.remove(int(op[1]))
+    if op[0] == "discard":
+        first_set.discard(int(op[1]))
+if b == 0:
+    c = str()
+    if first_set == set():
+        print("0")
+    else:
+        c = 0
+        for i in first_set:
+            c = c + int(i)
+        print(c)
