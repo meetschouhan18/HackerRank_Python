2755420d6cb8900dfda457b30cdc55d539f4144c
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-26 15:45:36 +0530

    Hacker Rank

--- SetSymmetricDifference.py
+++ SetSymmetricDifference.py
@@ -3,12 +3,16 @@ second_set = set()
 n = int(input())
 first_line = input()
 first_num = first_line.split()
+
 for i in first_num:
     first_set.add(int(i))
+    
 m = int(input())
 second_line = input()
 second_num = second_line.split()
+
 for i in second_num:
     second_set.add(int(i))
+    
 total = first_set.symmetric_difference(second_set)
 print(len(total))
