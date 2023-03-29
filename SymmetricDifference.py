751923ddfbce76a82060a232335830c79d0fc1f0
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-03-29 16:01:38 +0530

    Hacker Rank

--- SymmetricDifference.py
+++ SymmetricDifference.py
@@ -4,16 +4,23 @@ second_set = set()
 n = int(input())
 first_line = input()
 first_num = first_line.split()
+
 for i in first_num:
     first_set.add(int(i))
+    
 m = int(input())
 second_line = input()
 second_num = second_line.split()
+
 for i in second_num:
     second_set.add(int(i))
+    
 total = first_set.symmetric_difference(second_set)
+
 for i in total:
     l.append(i)
+    
 l.sort()
+
 for i in l:
     print(i)
